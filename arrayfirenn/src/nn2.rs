47ad8401a1df547aa30bf38d6d9b87f6658cdd9f
Robotik-PC <hannes.eilers@fh-kiel.de>
2018-11-20 14:37:12 +0100

    More conversion

--- arrayfirenn/src/nn2.rs
+++ arrayfirenn/src/nn2.rs
@@ -10,14 +10,7 @@ use af::*;
     
 }*/
 
-fn derivative(out: &Array<f32>) -> Array<f32>{
-    out * (constant(1, out.dims()) - out)
-}
 
-fn error(out: &Array<f32>, pred: &Array<f32>) -> f64{
-    let dif = out - pred;
-    sum_all(&pow(&dif, &2, true)).0.sqrt()
-}
 
 pub struct ANN{
     num_layers: u32,
@@ -31,6 +24,15 @@ impl ANN{
         join(1, &one_const , input)
     }
 
+    fn derivative(out: &Array<f32>) -> Array<f32>{
+        out * (constant(1, out.dims()) - out)
+    }
+
+    fn error(out: &Array<f32>, pred: &Array<f32>) -> f64{
+        let dif = out - pred;
+        sum_all(&pow(&dif, &2, true)).0.sqrt()
+    }
+
     fn forward_propagate(&self, input: &Array<f32>) -> Vec<Array<f32>>{
         let mut signal = Vec::with_capacity(self.num_layers as usize);
         signal[0] = input.copy(); //MAYBE THIS COPY DOESN'T WORK
@@ -42,11 +44,28 @@ impl ANN{
         signal
     }
 
-    fn back_propagate(&self, signal: Vec<Array<f32>>, target: &Array<f32>, alpha: f64){
-        let out = &signal[(self.num_layers - 1) as usize];
-        let error = out - target;
+    fn back_propagate(&mut self, signal: Vec<Array<f32>>, target: &Array<f32>, alpha: f64){
+        //Get error for output layer
+        let mut out = &signal[(self.num_layers - 1) as usize];
+        let mut error = out - target;
         let m = target.dims().get()[0];
 
-        for let i = 
+        for i in ((self.num_layers -2) as i32)..-1{
+            let input = ANN::add_bias(&signal[i as usize]);
+            let delta = transpose(&(ANN::derivative(out) * &error), false);
+            //Adjust weights
+            let grad = -(matmul(&delta, &input, MatProp::NONE, MatProp::NONE) * alpha) / m;
+            self.weights[i as usize] += transpose(&grad, false);
+
+            //Input to current layer is output of previous
+            out = &signal[i as usize];
+            error = matmul(&transpose(&delta, false), &transpose(&self.weights[i as usize], false), MatProp::NONE, MatProp::NONE);
+
+            //remove the error of bias and propagate backwards
+            error = error.cols(1,2);
+            error = ANN::error(span, seq(1, out.dims().get()[1]));
+        }
     }
+
+
 }
